3785cd62f543581a34476396bc6c79f19a02dbd7
a14 <a14@kali.a14>
2026-05-03 00:23:46 +0300

    fix services script

--- scripts/security/services.sh
+++ scripts/security/services.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+ssh_status=$(systemctl is-active ssh 2>/dev/null)
+cron_status=$(systemctl is-active cron 2>/dev/null)
+
+echo "SSH_STATUS=$ssh_status"
+echo "CRON_STATUS=$cron_status"
 # Security module for monitoring system services status
