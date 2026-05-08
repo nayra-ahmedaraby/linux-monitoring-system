af31a59cff221da09cd323287715a8b0580949b5
abdulrahman mohamed <Abdulrahman240101628@sut.edu.eg>
2026-05-08 05:28:19 +0300

    Update memory.sh

--- scripts/resources/memory.sh
+++ scripts/resources/memory.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-# Memory resource monitoring module
+# Memory monitoring module
+
+get_memory_usage() {
+    free | awk '/Mem:/ {printf("%.0f", $3/$2 * 100)}'
+}
+
+get_swap_usage() {
+    free | awk '/Swap:/ {
+        if ($2 == 0)
+            print 0
+        else
+            printf("%.0f", $3/$2 * 100)
+    }'
+}
+
+get_available_memory_mb() {
+    free -m | awk '/Mem:/ {print $7 " MB"}'
+}
