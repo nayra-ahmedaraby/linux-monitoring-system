000dc046d842f95cd1f4fe8484e3de7ca371bb44
abdulrahman mohamed <Abdulrahman240101628@sut.edu.eg>
2026-05-08 05:27:56 +0300

    Update load.sh

--- scripts/resources/load.sh
+++ scripts/resources/load.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-# System load average monitoring module
+# System load monitoring module
+
+get_load_average() {
+    uptime | awk -F'load average:' '{print $2}' | cut -d',' -f1 | xargs
+}
+
+get_uptime() {
+    uptime -p
+}
+
+get_logged_users() {
+    who | wc -l
+}
