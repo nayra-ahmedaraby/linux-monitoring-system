5181fc2453e75a261d8e9649ea8ecb3ea3baf3e2
abdulrahman mohamed <Abdulrahman240101628@sut.edu.eg>
2026-05-08 05:27:42 +0300

    Update disk.sh

--- scripts/resources/disk.sh
+++ scripts/resources/disk.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-# Disk resource monitoring module
+# Disk monitoring module
+
+get_disk_usage() {
+    df / | awk 'NR==2 {
+        gsub("%","",$5)
+        print $5
+    }'
+}
+
+get_disk_free_space() {
+    df -h / | awk 'NR==2 {print $4}'
+}
+
+get_largest_directory() {
+    du -h /home 2>/dev/null | sort -rh | head -1 | awk '{print $2}'
+}
