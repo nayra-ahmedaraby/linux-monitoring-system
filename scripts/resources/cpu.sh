3c32d6b2321e316e3e5f2230e0773dc5882f0efb
abdulrahman mohamed <Abdulrahman240101628@sut.edu.eg>
2026-05-08 05:27:26 +0300

    Enhance CPU monitoring script with additional functions
    Added functions to monitor CPU usage, temperature, and top process.

--- scripts/resources/cpu.sh
+++ scripts/resources/cpu.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-# CPU resource monitoring module
+# CPU monitoring module
+
+get_cpu_usage() {
+    top -bn1 | awk '/Cpu\(s\)/ {print int(100 - $8)}'
+}
+
+get_cpu_temperature() {
+    sensors 2>/dev/null | awk '/Package id 0:/ {print $4}' | head -1
+}
+
+get_top_cpu_process() {
+    ps -eo comm,%cpu --sort=-%cpu | awk 'NR==2 {print $1 " (" $2 "%)"}'
+}
